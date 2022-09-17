42ac29755aed135fbbabfcf22bbda5e7c05ec7e5
Jonathan Face <jonathan@masonhub.co>
2022-09-16 21:49:12 -0500

    fixed routing

--- main.go
+++ main.go
@@ -10,10 +10,6 @@ func main() {
 	r.RedirectTrailingSlash = true
 	r.SetTrustedProxies([]string{"192.168.0.1"})
 	r.Use(static.Serve("/", static.LocalFile("./ui/public", false)))
-	r.Use(static.Serve("/news", static.LocalFile("./ui/public", false)))
-	r.Use(static.Serve("/books", static.LocalFile("./ui/public", false)))
-	r.Use(static.Serve("/about", static.LocalFile("./ui/public", false)))
-	r.Use(static.Serve("/contact", static.LocalFile("./ui/public", false)))
 	r.NoRoute(func(c *gin.Context) {
 		c.File("./ui/public/index.html")
 	})
