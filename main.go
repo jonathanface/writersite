7a3a1d9bb8b4cdfbbb12fded1135738d1245fa48
Jonathan Face <jonathan.face@protonmail.com>
2025-04-23 14:25:37 -0500

    fixed port

--- main.go
+++ main.go
@@ -2,20 +2,31 @@ package main
 
 import (
 	"jonathanface/api"
+	"jonathanface/models"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 const (
 	uiDirectory = "./ui/public"
-	port        = ":8443"
 	apiPath     = "/api"
 )
 
 func main() {
+	currentMode := models.AppMode(strings.ToLower(os.Getenv("MODE")))
+	if currentMode != models.ModeProduction && currentMode != models.ModeStaging {
+		if err := godotenv.Load(); err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+	port := os.Getenv("PORT")
 	r := gin.Default()
 	r.RedirectTrailingSlash = true
 	r.SetTrustedProxies([]string{"192.168.0.1"})
